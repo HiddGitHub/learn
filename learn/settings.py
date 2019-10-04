605c422a61553c734240139ca617a46d0c25acab
HiddGitHub <17748646960@163.com>
2019-10-04 17:27:30 +0800

    1

--- learn/settings.py
+++ learn/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'learn.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'USER': 'root',
+         'PASSWORD': '1213',
+         'HOST': '127.0.0.1',
+         'PORT': 3306,
+     }
 }
 
 
